ec73f7b145c1bffdb188837e50479f7a17599587
Vingnir <vingnir@live.se>
2022-03-17 19:23:05 +0100

    added menu sidebar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Components/Sidebar';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home';
+import People from './Pages/People';
+import Languages from './Pages/Languages';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/languages" element={<Languages />} />
+        <Route path="*" element={<p>There's nothing here: 404!</p>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/SidebarData.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/home",
+    icon: <AiIcons.AiFillHome />,
+    cName: "nav-text",
+  },
+  {
+    title: "People",
+    path: "/people",
+    icon: <IoIcons.IoMdPeople />,
+    cName: "nav-text",
+  },
+  {
+    title: "Languages",
+    path: "/languages",
+    icon: <IoIcons.IoMdPeople />,
+    cName: "nav-text",
+  },
+];
